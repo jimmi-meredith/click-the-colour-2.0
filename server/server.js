dfece952ae9d9c8a969c7c761390b742bd9296f1
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-06-08 15:12:21 +1200

    configure server a bit more

--- server/server.js
+++ server/server.js
@@ -1,9 +1,16 @@
 const express = require('express')
-
+const path = require('path')
+const cors = require('cors')
 const server = express()
 
-server.use(express.json())
-
 server.use('/', require('./routes'))
+
+// accepts any api call from any ip address
+server.use(cors())
+// needed for converting POST data into json and stores it into req.body
+server.use(express.urlencoded({ extended: false }))
+server.use(express.json())
+// automatically sets path when importing stuff into index.html
+server.use(express.static(path.resolve('server', 'public')))
 
 module.exports = server
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: './client/index.js',
+  output: {
+    path: path.resolve('server', 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg|png|jpe?g|gif)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: './server/public'
+  }
+}
