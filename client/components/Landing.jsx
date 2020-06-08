26d75faa1abc1bd17a014e0f5194445078aeb584
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-06-08 16:51:38 +1200

    create LandingContent component

--- client/components/Landing.jsx
+++ client/components/Landing.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+
 import Header from './Header'
+import LandingContent from './LandingContent'
 
 const Landing = () => {
   return (
     <>
     <Header />
     <div className='main-container'>
+      <LandingContent />
     </div>
     </>
   )
---
+++ client/components/LandingContent.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const LandingContent = () => {
+  return (
+    <>
+    <div className='introduction'>
+      <p>Race against the Clock!</p>
+      <p>See how many colours you can click. All you have to do is click the <strong>colour of the word.</strong></p>
+      <p>Not the word, but the colour... Get it? Cool.</p>
+      <p>Get clicking!</p>
+    </div>
+    <div className='start-button'>
+      <button>Start Game</button>
+    </div>
+    </>
+  )
+}
+
+export default LandingContent
