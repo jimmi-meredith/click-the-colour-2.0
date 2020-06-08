9de123eb6c1ce58bef209adcae8aadcc522441fb
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-06-08 15:29:08 +1200

    Set up ready for react

---
+++ client/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <div>
+      HOME PAGE
+    </div>,
+    document.getElementById('app')
+  )
+})
