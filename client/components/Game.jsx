4a97c5da86e7a581a2feb7f4ecbe49784a067e09
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-06-09 16:04:42 +1200

    Write functionaly for generating random game word and colour

--- client/components/Game.jsx
+++ client/components/Game.jsx
@@ -1,26 +1,38 @@
 import React, { useState } from 'react'
 
 const Game = () => {
+  const colours = ['purple', 'red', 'yellow', 'black', 'blue', 'green']
   // useing react hooks to set the state of the timer
   // setTimer is used to set the state value of timer
   const [timer, setTimer] = useState(15)
   const [gameWord, setGameWord] = useState('')
-  const colours = ['purple', 'red', 'yellow', 'black', 'blue', 'green']
+  const [wordColour, setWordColour] = useState('')
+
+  function getRandomNumber (array) {
+    return Math.floor(Math.random() * array.length)
+  }
+
+  function getGameWord () {
+    let wordWord = colours[getRandomNumber(colours)]
+    getWordColour(wordWord)
+  }
+
+  function getWordColour (word) {
+    let newColours = colours.filter(colour => colour !== word)
+    setWordColour(newColours[getRandomNumber(newColours)])
+    setGameWord(word)
+  }
 
   return (
     <div className='game-content'>
       <div className='timer'>
         {timer}
       </div>
-      <div className='game-word'>
+      <div className={wordColour}>
+        {gameWord}
       </div>
       <div className='game-buttons'>
-        <button>Black</button>
-        <button>Blue</button>
-        <button>Green</button>
-        <button>Purple</button>
-        <button>Red</button>
-        <button>Yellow</button>
+        {colours.map(colour => <button key={colour} onClick={getGameWord}>{colour}</button>)}
       </div>
     </div>
   )
