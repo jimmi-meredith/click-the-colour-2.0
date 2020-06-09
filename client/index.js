9a3be4d82bfd8503317b36c7ed73fc1266f2908d
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-06-09 17:17:54 +1200

    create redux store

--- client/index.js
+++ client/index.js
@@ -1,11 +1,21 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 
 import App from './components/App'
 
+const store = createStore(reducers,
+  // this is how we wire in the devtools in the browser (also important)
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
 document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(
-    <App/>,
+  render(
+    // passing the sotre as props into provider so it can be used in everything in the app
+    <Provider store={store}>
+      <App />
+    </Provider>,
     document.getElementById('app')
   )
 })
