40588f32c1bcb7e0b77eba1fb071fd44d8888402
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-06-09 17:31:34 +1200

    impliment redux for gameState

--- client/reducers/gameState.js
+++ client/reducers/gameState.js
@@ -1,6 +1,8 @@
+import { SET_GAME_STATE } from '../actions'
+
 const gameState = (state = '', action) => {
   switch (action.type) {
-    case 'SET_GAME_STATE':
+    case SET_GAME_STATE:
       return action.gameState
     default:
       return state
--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,5 +1,7 @@
 import { combineReducers } from 'redux'
 
+import gameState from './gameState'
+
 export default combineReducers({
-  
+  gameState
 })
