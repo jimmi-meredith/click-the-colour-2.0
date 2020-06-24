45d609a5124af64951fb9399e124e67f70724a14
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-06-24 14:19:42 +1200

    add home as default gameState

--- client/reducers/gameState.js
+++ client/reducers/gameState.js
@@ -1,6 +1,6 @@
 import { SET_GAME_STATE } from '../actions'
 
-const gameState = (state = '', action) => {
+const gameState = (state = 'home', action) => {
   switch (action.type) {
     case SET_GAME_STATE:
       return action.gameState
