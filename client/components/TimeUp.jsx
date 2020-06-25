c30bfc33acd892cbe38d7424bf44917c7589b48c
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-06-25 14:06:39 +1200

    add basics for timeUp component

--- client/components/TimeUp.jsx
+++ client/components/TimeUp.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const TimeUp = () => {
   return (
     <div className='time-up'>
-
+      <h1>Time is up!</h1>
+      <p>Your final score is </p>
+      <button>Try Again</button>
     </div>
   )
 }
