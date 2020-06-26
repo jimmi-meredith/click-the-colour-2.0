4ed76b1fd0976eb0f80f4b0ebb7bb5181faf75ac
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-06-26 15:05:52 +1200

    add onclick for try again button in timeUp component

--- client/components/TimeUp.jsx
+++ client/components/TimeUp.jsx
@@ -3,12 +3,12 @@ import { connect } from 'react-redux'
 
 import { setGameState } from '../actions'
 
-const TimeUp = () => {
+const TimeUp = props => {
   return (
     <div className='time-up'>
       <h1 className='heading'>Time is up!</h1>
       <p className='score'>Your final score is </p>
-      <button className='start-button'>Try Again</button>
+      <button className='start-button' onClick={() => props.setGameState('game')}>Try Again</button>
     </div>
   )
 }
