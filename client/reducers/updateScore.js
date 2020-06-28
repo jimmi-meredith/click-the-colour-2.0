d37d6898a674a6b2e5107f627177ba7272801c06
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-06-29 09:41:07 +1200

    finish updateScore reducer

--- client/reducers/updateScore.js
+++ client/reducers/updateScore.js
@@ -1,7 +1,12 @@
 import { UPDATE_SCORE } from '../actions'
 
 const updateScore = (state = '', action) => {
-
+  switch (action.type) {
+    case UPDATE_SCORE:
+      return action.score
+    default:
+      return state
+  }
 }
 
 export default updateScore
