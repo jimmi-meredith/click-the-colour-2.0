c981029a03b9c351e6622bd88b014c6e2c55760f
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-06-29 09:42:27 +1200

    add updateScore reducer in combined reducers

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux'
 
 import gameState from './gameState'
+import updateScore from './updateScore'
 
 export default combineReducers({
-  gameState
+  gameState,
+  updateScore
 })
