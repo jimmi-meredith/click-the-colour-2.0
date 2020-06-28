22cdd5320846c72b6cbe969d19be236ac09cfa2a
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-06-29 09:34:11 +1200

    add onClick for try again button

--- client/components/Incorrect.jsx
+++ client/components/Incorrect.jsx
@@ -3,12 +3,12 @@ import { connect } from 'react-redux'
 
 import { setGameState } from '../actions'
 
-const Incorrect = () => {
+const Incorrect = props => {
   return (
     <div className='incorrect'>
       <h1 className='heading'>Oops!</h1>
       <p className='score'>Your final score is </p>
-      <button className='start-button'>Try Again</button>
+      <button className='start-button' onClick={() => props.setGameState('game')}>Try Again</button>
     </div>
   )
 }
