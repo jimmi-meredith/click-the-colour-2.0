f3254cb4472dc464dfe3e759004770976c01d1f6
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-07-06 10:42:46 +1200

    add resetScore reducer

--- client/reducers/score.js
+++ client/reducers/score.js
@@ -4,6 +4,8 @@ const score = (state = 0, action) => {
   switch (action.type) {
     case UPDATE_SCORE:
       return state + 1
+    case RESET_SCORE:
+      return state
     default:
       return state
   }
