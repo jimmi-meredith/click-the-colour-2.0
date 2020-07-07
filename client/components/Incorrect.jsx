830b03bbb0928e1a64cec07b3351ba7d7c38c098
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-07-08 09:59:48 +1200

    display total score in incorrect component

--- client/components/Incorrect.jsx
+++ client/components/Incorrect.jsx
@@ -7,7 +7,7 @@ const Incorrect = props => {
   return (
     <div className='incorrect'>
       <h1 className='heading'>Oops!</h1>
-      <p className='score'>Your final score is </p>
+      <p className='score'>Your final score is {props.score}</p>
       <button className='start-button' onClick={() => props.setGameState('game')}>Try Again</button>
     </div>
   )
