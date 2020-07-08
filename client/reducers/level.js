e9c3b9b8a1b2eab44fac5627cc9d7df0113d9fff
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-07-09 09:21:49 +1200

    create level reducer

--- client/reducers/level.js
+++ client/reducers/level.js
@@ -1,1 +1,12 @@
 import { UPDATE_LEVEL } from '../actions'
+
+const level = (state = 1, action) => {
+  switch (action.type) {
+    case UPDATE_LEVEL:
+      return state + 1
+    default:
+      return state
+  }
+}
+
+export default level
