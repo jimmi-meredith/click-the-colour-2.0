8f199b2c903e093a89c913e7c1c93c1d9e6a2631
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-07-09 09:22:35 +1200

    import level into combined reducers

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux'
 
 import gameState from './gameState'
 import score from './score'
+import level from './level'
 
 export default combineReducers({
   gameState,
